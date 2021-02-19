788022132bd9ead7b4d5e9142fc00494e8efeec8
NErgezinger <noah.ergezinger@gmail.com>
2021-02-19 13:24:26 -0700

    Fixed indentation

--- social_distribution/Profile/views.py
+++ social_distribution/Profile/views.py
@@ -16,7 +16,7 @@ def home(request):
 	-------
 	Render to the home.html
 	"""
-    return render(request, 'profile/home.html')
+	return render(request, 'profile/home.html')
 
 @login_required(login_url='/login/')
 def update_profile(request):
@@ -29,23 +29,23 @@ def update_profile(request):
 	-------
 	Either submit the form if POST or view the form.
 	"""
-    if request.method == 'POST':
-        user_form = UserForm(request.POST, instance=request.user)
-        profile_form = ProfileForm(request.POST, instance=request.user.profile)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Your profile was successfully updated!')
-            return redirect('Profile:home')
-        else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        user_form = UserForm(instance=request.user)
-        profile_form = ProfileForm(instance=request.user.profile)
-    return render(request, 'profile/profile.html', {
-        'user_form': user_form,
-        'profile_form': profile_form
-    })
+	if request.method == 'POST':
+		user_form = UserForm(request.POST, instance=request.user)
+		profile_form = ProfileForm(request.POST, instance=request.user.profile)
+		if user_form.is_valid() and profile_form.is_valid():
+			user_form.save()
+			profile_form.save()
+			messages.success(request, 'Your profile was successfully updated!')
+			return redirect('Profile:home')
+		else:
+			messages.error(request, 'Please correct the error below.')
+	else:
+		user_form = UserForm(instance=request.user)
+		profile_form = ProfileForm(instance=request.user.profile)
+	return render(request, 'profile/profile.html', {
+		'user_form': user_form,
+		'profile_form': profile_form
+	})
 
 def signup(request):
 	"""
@@ -57,17 +57,17 @@ def signup(request):
 	Returns
 	-------
 	"""
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            messages.success(request, 'Your user was successfully created!')
-            return redirect('Profile:profile')
-    else:
-        form = SignUpForm()
-    return render(request, 'profile/signup.html', {'form': form})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			user.refresh_from_db()  # load the profile instance created by the signal
+			user.save()
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=user.username, password=raw_password)
+			login(request, user)
+			messages.success(request, 'Your user was successfully created!')
+			return redirect('Profile:profile')
+	else:
+		form = SignUpForm()
+	return render(request, 'profile/signup.html', {'form': form})
