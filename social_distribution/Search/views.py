50a86d8cee1844bf0021b230d76748e4411e62f6
gskeates <gskeates@ualberta.ca>
2021-03-04 21:26:45 -0700

    Fixes search

--- social_distribution/Search/views.py
+++ social_distribution/Search/views.py
@@ -10,5 +10,5 @@ from Profile.models import Author
 # Create your views here.
 def results(request):
     query = request.POST.get('query', '')
-    users = User.objects.filter(username__contains=query)
-    return render(request, 'results.html', {'query': query, 'users': users})
+    authors = Author.objects.filter(user__username__contains=query)
+    return render(request, 'results.html', {'query': query, 'authors': authors})
