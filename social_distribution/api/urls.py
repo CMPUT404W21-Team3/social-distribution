23e3741790ad05e48c282eabfaf087225f441568
gskeates <gskeates@ualberta.ca>
2021-03-04 23:26:51 -0700

    Updates API

--- social_distribution/api/urls.py
+++ social_distribution/api/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('author/<str:author_id>/', views.get_author),
     path('author/<str:author_id>/posts/<str:post_id>', views.get_post),
+    path('api/author/<str:author_id>/', views.get_author),
+    path('api/author/<str:author_id>/posts/<str:post_id>', views.get_post),
 ]
