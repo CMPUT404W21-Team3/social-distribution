c491d088a162861a7a7f2524102b833b957d21ca
NErgezinger <noah.ergezinger@gmail.com>
2021-03-24 19:16:43 -0600

    remove script from home and remote search

--- social_distribution/Search/views.py
+++ social_distribution/Search/views.py
@@ -1,14 +1,29 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+import requests
+import json
 
 from .forms import SearchForm
 from .models import FriendRequest
 
 from Profile.models import Author
+from api.models import Connection
 
 # Create your views here.
 def results(request):
     query = request.POST.get('query', '')
     authors = Author.objects.filter(user__username__contains=query)
+
+    remote_authors = []
+
+    for connection in Connection.objects.all():
+        url = connection.url + 'service/authors'
+        response = requests.get(url)
+        for author in response.json()['items']:
+            print(author)
+            remote_authors.append(Author.objects.create(**author))
+
+    authors += remote_authors
+
     return render(request, 'results.html', {'query': query, 'authors': authors})
