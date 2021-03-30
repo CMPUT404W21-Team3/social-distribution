78544d3028df5e960dc255c3a9be8f31755c44a9
gskeates <gskeates@ualberta.ca>
2021-03-29 20:53:20 -0600

    Adds comment field to post id

--- social_distribution/api/serializers.py
+++ social_distribution/api/serializers.py
@@ -16,14 +16,14 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('type', 'title', 'id', 'source', 'origin', 'description',
-                    'content_type', 'content', 'author', 'categories', 'timestamp',
+                    'content_type', 'content', 'author', 'comments', 'categories', 'timestamp',
                     'visibility', 'unlisted')
 
     # https://stackoverflow.com/questions/41312558/django-rest-framework-post-nested-objects
     def to_representation(self, instance):
         data = super().to_representation(instance)
         data['author'] = AuthorSerializer(Author.objects.get(pk=data['author'])).data
-        #data['comments'] = CommentSerializer(Comment.objects.filter(id__in=data['comments']), many=True).data
+        data['comments'] = CommentSerializer(Comment.objects.filter(id__in=data['comments']), many=True).data
         return data
 
     # https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/
