3540d71664b584ba151a819a428f50918356e205
gskeates <gskeates@ualberta.ca>
2021-03-30 00:55:38 -0600

    Finishes API

--- social_distribution/api/urls.py
+++ social_distribution/api/urls.py
@@ -5,10 +5,13 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-  
     path('author/<str:author_id>/', views.author, name='author'),
     path('author/<str:author_id>/posts/<str:post_id>', views.post),
     path('author/<str:author_id>/posts/', views.posts),
+    path('author/<str:author_id>/friends/', views.friends),
+    path('author/<str:author_id>/friends/<str:friend_id>', views.friend),
+    path('author/<str:author_id>/friendrequests/', views.requests),
+    path('author/<str:author_id>/friendrequests/<str:sender_id>', views.request),
     path('author/<str:author_id>/followers', views.followers),
     path('author/<str:author_id>/followers/<str:follower_id>', views.follower),
     path('author/<str:author_id>/posts/<str:post_id>/comments', views.comments),
@@ -22,6 +25,10 @@ urlpatterns = [
     path('api/author/<str:author_id>/', views.author),
     path('api/author/<str:author_id>/posts/<str:post_id>', views.post),
     path('api/author/<str:author_id>/posts/', views.posts),
+    path('api/author/<str:author_id>/friends/', views.friends),
+    path('api/author/<str:author_id>/friends/<str:friend_id>', views.friend),
+    path('api/author/<str:author_id>/friendrequests/', views.requests),
+    path('api/author/<str:author_id>/friendrequests/<str:sender_id>', views.request),
     path('api/author/<str:author_id>/followers', views.followers),
     path('api/author/<str:author_id>/followers/<str:follower_id>', views.follower),
     path('api/author/<str:author_id>/posts/<str:post_id>/comments', views.comments),
