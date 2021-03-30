42938fea3d40d44fb24d485b475888e1209a5297
gskeates <gskeates@ualberta.ca>
2021-03-30 15:33:06 -0600

    Update models.py

--- social_distribution/Profile/models.py
+++ social_distribution/Profile/models.py
@@ -2,6 +2,7 @@ import uuid, commonmark
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone
