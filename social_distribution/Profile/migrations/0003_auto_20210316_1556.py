b1d14d43a2e93b7d71a72459174266641e054770
gskeates <gskeates@ualberta.ca>
2021-04-09 20:29:08 -0600

    Update 0003_auto_20210316_1556.py

--- social_distribution/Profile/migrations/0003_auto_20210316_1556.py
+++ social_distribution/Profile/migrations/0003_auto_20210316_1556.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('Search', '0001_initial'),
         ('Profile', '0001_initial'),
     ]
