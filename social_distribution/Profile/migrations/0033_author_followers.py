a98e9d435b6637e3e6dcd87b78a73c361141fb9a
gskeates <gskeates@ualberta.ca>
2021-04-11 21:20:08 -0600

    Update 0033_author_followers.py

--- social_distribution/Profile/migrations/0033_author_followers.py
+++ social_distribution/Profile/migrations/0033_author_followers.py
@@ -10,9 +10,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='author',
-            name='followers',
-            field=models.ManyToManyField(related_name='follower_list', to='Profile.Author'),
-        ),
     ]
